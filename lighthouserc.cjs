020972497099c954ac3ca133c7300c84af6de5c7
ceejaymar <martinezcj2@gmail.com>
2026-06-19 19:22:28 -0400

    initial commit

---
+++ lighthouserc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: "pnpm start",
+      startServerReadyPattern: "ready",
+      url: [
+        "http://localhost:3000/",
+        "http://localhost:3000/work",
+        "http://localhost:3000/archive",
+      ],
+      numberOfRuns: 3,
+    },
+    assert: {
+      assertions: {
+        "categories:accessibility": ["error", { minScore: 0.95 }],
+        "categories:best-practices": ["error", { minScore: 0.95 }],
+        "categories:seo": ["error", { minScore: 0.95 }],
+        "categories:performance": ["warn", { minScore: 0.85 }],
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
