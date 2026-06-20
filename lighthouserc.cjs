4dbb346ca9ba7b06920918ecf62055f3827bb610
ceejaymar <martinezcj2@gmail.com>
2026-06-19 20:54:13 -0400

    setup biome & lefthook

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,25 +1,25 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: "pnpm start",
-      startServerReadyPattern: "ready",
+      startServerCommand: 'pnpm start',
+      startServerReadyPattern: 'ready',
       url: [
-        "http://localhost:3000/",
-        "http://localhost:3000/work",
-        "http://localhost:3000/archive",
+        'http://localhost:3000/',
+        'http://localhost:3000/work',
+        'http://localhost:3000/archive',
       ],
       numberOfRuns: 3,
     },
     assert: {
       assertions: {
-        "categories:accessibility": ["error", { minScore: 0.95 }],
-        "categories:best-practices": ["error", { minScore: 0.95 }],
-        "categories:seo": ["error", { minScore: 0.95 }],
-        "categories:performance": ["warn", { minScore: 0.85 }],
+        'categories:accessibility': ['error', { minScore: 0.95 }],
+        'categories:best-practices': ['error', { minScore: 0.95 }],
+        'categories:seo': ['error', { minScore: 0.95 }],
+        'categories:performance': ['warn', { minScore: 0.85 }],
       },
     },
     upload: {
-      target: "temporary-public-storage",
+      target: 'temporary-public-storage',
     },
   },
 };
