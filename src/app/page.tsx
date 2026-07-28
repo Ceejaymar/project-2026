7efc5420a36940493906c1176b44f2bda0a85e03
ceejaymar <martinezcj2@gmail.com>
2026-07-28 01:06:06 -0400

    adjust projects section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import About from './_sections/about/about-section';
 import Contact from './_sections/contact/contact-section';
-import CraftSection from './_sections/craft/craft-section';
+// import CraftSection from './_sections/craft/craft-section';
 import Hero from './_sections/hero/hero';
 import Projects from './_sections/projects/projects-section';
 import styles from './page.module.css';
