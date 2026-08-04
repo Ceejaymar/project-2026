0d1660654481ae6de201eda5aa3a0fc407f2002c
ceejaymar <martinezcj2@gmail.com>
2026-08-03 21:45:33 -0400

    re add expandable iamge

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
   allowedDevOrigins: ['192.168.1.201'],
   images: {
@@ -17,6 +16,9 @@ const nextConfig: NextConfig = {
         pathname: '/portfolio-2026/**',
       },
     ],
+  },
+  experimental: {
+    viewTransition: true,
   },
 };
 
